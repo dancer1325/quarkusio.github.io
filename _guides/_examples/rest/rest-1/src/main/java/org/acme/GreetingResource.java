0bd25360efa4f8abc60901a7779367a9bed62022
dancer1325 <alfredotic0809@gmail.com>
2025-11-14 15:13:58 +0100

    docs: clear notes and add examples

---
+++ _guides/_examples/rest/rest-1/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/withoutSpecifyingProduce")
+    public String helloWithoutSpecifyingProduce() {
+        return "Hello from Quarkus REST";
+    }
+}
